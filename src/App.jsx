2244f41e29cac838df264f0d548f8d78f6fe5ef7
Ndekocode <arickbulakali@gmail.com>
2023-01-14 22:12:38 +0200

    add dynamic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
 import Todos from "./pages/Todos";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/todos" element={<Todos />} />
-      <Route path="/gallery" element={<Gallery />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </>
   );
 };
 
